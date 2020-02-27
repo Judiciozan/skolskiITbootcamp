22bb457bad995c9c241b11dfb509d30d0ab4b20e
Judiciozan <petrovic.aleksandar.256@gmail.com>
2020-02-27 17:05:52 +0100

    Pocetak DOM

---
+++ DOM/script.js
@@ -0,0 +1,83 @@
+// query selektor po imenu taga
+let paragraf = document.querySelector('p');   // Selektuje samo prvi element sa odgovarajucem tagom
+console.log(paragraf);
+let div = document.querySelector('div');
+console.log(div);
+
+
+// query selektor po imenu klase
+let klasaTest = document.querySelector(".test"); 
+console.log(klasaTest);
+
+let klasaTestParagraf = document.querySelector("p.test"); 
+console.log(klasaTestParagraf);
+
+let treciParagraf = document.querySelector("#treci_paragraf");
+console.log(treciParagraf);
+
+
+// Querry selektor za sve <p> tagove
+let sviParagrafi = document.querySelectorAll('p');
+console.log(sviParagrafi);
+
+// Pristupanje elementu Node liste
+console.log(sviParagrafi[0]);
+
+// Kretanje kroz Node listu
+
+sviParagrafi.forEach(elem =>{
+    console.log(elem);
+});
+
+//
+let klasaTestDoc = document.getElementsByClassName('test');
+console.log(klasaTestDoc);
+// neuspelo kretanje kroz forEach petljom kroz HTML koleciju
+//klasaTestDoc.forEach(elem => {
+  //  console.log(elem);
+//});
+
+console.log(klasaTestDoc[0]);
+// ovde je kretanje HTML kolekcijom FOR petljom
+for (let i = 0; i < klasaTestDoc.length; i++) {
+    console.log (klasaTestDoc[i]);
+}
+
+// InnerText 
+paragraf.innerText += "Zdravo ";
+paragraf.innerText += "Svete";
+
+paragraf.innerHTML += "<span> Kako si? </span>";
+
+//Vezbanje 
+// Selektovati sve paragrafe i  u svakom od njih pridodati tekst "Vazno!"
+
+sviParagrafi.forEach(elem => {
+    elem.innerHTML += " " +"<b>VAZNO!</b>";
+});
+
+// 
+sviParagrafi.forEach((elem, index) => {
+    let kv = (index + 1) ** 2;
+    elem.innerText += `${kv}`;
+});
+
+/* Napraviti niz od najmanje tri imena.
+Proći nizom i imena ispisati:
+    1. Svako u novom paragrafu
+    2. U listi kao stavke liste
+    3. U jednoj koloni tabele*/
+//1.
+let imena = ["Jovana", "Miljana", "Milena"];
+sviParagrafi.forEach((elem, index) => {
+    elem.innerText += imena[index];
+});
+//2.
+let lista = document.querySelector('ul');
+imena.forEach(elem => {
+    let item = `<li> ${elem} </li>`;
+    lista.innerHTML += item;
+
+});
+
+
